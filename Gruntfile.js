b538c47c8f9658c2ad45483120ffa504380d5219
Philippe Masset <pioul06+github@gmail.com>
2017-01-20 17:11:04 +0100

    Update Firefox extension build (transition to WebExt)

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,31 +3,8 @@ var path = require('path');
 
 var CONFIG_FILE = {
   CHROME:  'chrome/chrome/manifest.json',
-  FIREFOX: {
-    SRC: 'firefox/firefox/src/package.json',
-    DIST: 'firefox/firefox/dist/package.json'
-  },
   SAFARI:  'safari/safari/buffer.safariextension/Info.plist'
 };
-
-// This config is to switch parameters in the package.json between the
-// self-hosted and Mozilla Add-on site hosted extension
-var FIREFOX_CONFIG = {
-  HOSTED: {
-    title: "Buffer for Firefox",
-    id: "firefox@buffer",
-    name: "buffer-for-firefox",
-    icon: "resource://firefox-at-buffer/data/shared/img/Icon.png",
-    updateURL: "https://s3.amazonaws.com/buffer-static/extensions/firefox/buffer.update.rdf"
-  },
-  ADDON: {
-    title: "Buffer",
-    id: "jid1-zUyU7TGKwejAyA@jetpack",
-    name: "buffer",
-    icon: "resource://jid1-zUyU7TGKwejAyA-at-jetpack/data/shared/img/Icon.png"
-  }
-};
-
 
 module.exports = function(grunt) {
 
@@ -36,10 +13,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     pkg: pkg,
-
-    FIREFOX_CONFIG: FIREFOX_CONFIG,
-
-    FIREFOX_STATIC_DIR: 'https://s3.amazonaws.com/buffer-static/extensions/firefox/',
 
     shell: {
       chrome: {
@@ -52,36 +25,14 @@ module.exports = function(grunt) {
           'cd ../'
         ].join('&&')
       },
-      'firefox-hosted': {
+      firefox: {
         options: {
           stdout: true
         },
         command: [
-          'cd firefox/firefox/src',
-          'jpm xpi',
-          'mv <%= FIREFOX_CONFIG.HOSTED.id %>-<%= pkg.version %>.xpi ../../releases/buffer-<%= pkg.version %>.xpi',
-          'cd ../../../'
-        ].join('&&')
-      },
-      'firefox-addon': {
-        options: {
-          stdout: true
-        },
-        command: [
-          'cd firefox/firefox/src',
-          'jpm xpi',
-          'mv <%= FIREFOX_CONFIG.ADDON.id %>-<%= pkg.version %>.xpi ../../releases/buffer-<%= pkg.version %>-addon-edition.xpi',
-          'cd ../../../'
-        ].join('&&')
-      },
-      firefox_test: {
-        options: {
-          stdout: true
-        },
-        command: [
-          'cd firefox/firefox/src',
-          'jpm test',
-          'cd ../../../'
+          'cd chrome/chrome',
+          'zip -r ../releases/firefox-<%= pkg.version %>.zip .',
+          'cd ../../'
         ].join('&&')
       },
 
@@ -91,8 +42,6 @@ module.exports = function(grunt) {
         },
         command: [
           'cd ' + path.join(__dirname, 'chrome'),
-          'git pull',
-          'cd ' + path.join(__dirname, 'firefox'),
           'git pull',
           'cd ' + path.join(__dirname, 'safari'),
           'git pull'
@@ -105,8 +54,6 @@ module.exports = function(grunt) {
         },
         command: [
           'cd ' + path.join(__dirname, 'chrome/chrome/data/shared'),
-          'git pull',
-          'cd ' + path.join(__dirname, 'firefox/firefox/src/data/shared'),
           'git pull',
           'cd ' + path.join(__dirname, 'safari/safari/buffer.safariextension/data/shared'),
           'git pull'
@@ -122,26 +69,6 @@ module.exports = function(grunt) {
         run: true
       }
     },
-
-    concat: {
-      firefoxBackgroundPage: {
-        src: ['firefox/firefox/dist/lib/main.js', 'firefox/firefox/dist/data/shared/buffermetrics-bg-shim.js', 'firefox/firefox/dist/data/shared/buffermetrics.js'],
-        dest: 'firefox/firefox/dist/lib/main.js'
-      }
-    },
-
-    copy: {
-      firefoxSrcToDist: {
-        cwd: 'firefox/firefox/src/',
-        src: '**',
-        dest: 'firefox/firefox/dist/',
-        expand: true
-      }
-    },
-
-    clean: {
-      firefoxDist: 'firefox/firefox/dist/'
-    }
   });
 
   // Warns the developer to bump the version number if there is already a build
@@ -150,8 +77,7 @@ module.exports = function(grunt) {
 
     var paths = {
       chrome:  'chrome/releases/chrome-' + pkg.version + '.zip',
-      firefox: 'firefox/releases/buffer-' + pkg.version + '.xpi',
-      firefox_addon: 'firefox/releases/buffer-' + pkg.version + '-addon-edition.xpi'
+      firefox: 'chrome/releases/firefox-' + pkg.version + '.zip',
     };
 
     if (browser in paths && !grunt.file.exists( paths[ browser ] )){
@@ -172,9 +98,9 @@ module.exports = function(grunt) {
     },
 
     firefox: function(version) {
-      var firefoxConfig = grunt.file.readJSON( CONFIG_FILE.FIREFOX.SRC );
+      var firefoxConfig = grunt.file.readJSON( CONFIG_FILE.CHROME );
       firefoxConfig.version = version;
-      grunt.file.write(CONFIG_FILE.FIREFOX.SRC, JSON.stringify(firefoxConfig, null, '  '));
+      grunt.file.write(CONFIG_FILE.CHROME, JSON.stringify(firefoxConfig, null, '  '));
     },
 
     safari: function(version) {
@@ -210,27 +136,6 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.registerTask('set-firefox-config',
-    'Updates the Firefox config package.json for a specific build', function(version) {
-
-    // The addon store version is the default in the repo
-    if (!version) version = 'ADDON';
-    version = version.toUpperCase();
-
-    if (Object.keys(FIREFOX_CONFIG).indexOf(version) === -1)
-      throw new Error('"' + version +'" is not a valid configuration key');
-
-    var firefoxConfig = grunt.file.readJSON( CONFIG_FILE.FIREFOX.DIST );
-
-    for (var key in FIREFOX_CONFIG[version]) {
-      firefoxConfig[key] = FIREFOX_CONFIG[version][key];
-    }
-
-    grunt.file.write(CONFIG_FILE.FIREFOX.SRC, JSON.stringify(firefoxConfig, null, '  '));
-
-    grunt.log.ok('Firefox package.json successfully updated to ' + version);
-  });
-
   //  Load Shell commands plugin
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-mocha');
@@ -251,31 +156,11 @@ module.exports = function(grunt) {
     'shell:chrome'
   ]);
 
-  grunt.registerTask('firefox-hosted', 'Build the hosted firefox extension', [
-    'version-exists:firefox',
-    'set-firefox-config:hosted',
-    'shell:firefox-hosted'
-  ]);
-
-  grunt.registerTask('firefox-addon', 'Build the addon listed firefox extension', [
-    'version-exists:firefox_addon',
-    'set-firefox-config:addon',
-    'shell:firefox-addon'
-  ]);
-
-
   grunt.registerTask('firefox', 'Build the firefox extension', [
     'mocha',
     'update-versions:firefox',
-    'clean:firefoxDist',
-    'copy:firefoxSrcToDist',
-    'concat:firefoxBackgroundPage',
-    'firefox-hosted',
-    'firefox-addon'
-  ]);
-
-  grunt.registerTask('firefox-test', 'Test the build in firefox', [
-    'shell:firefox_test'
+    'version-exists:firefox',
+    'shell:firefox'
   ]);
 
   grunt.registerTask('update-shared', 'Pull shared repo in all extensions', [
